1504fea181509e85ee5a75b1096573ed29ed648a
Austin File <austinfile510@gmail.com>
2020-09-02 16:07:47 -0700

    added error message for being unable to find breed

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 function getDogImage(dogBreed) {
-    fetch (`https://dog.ceo/api/breed/${dogBreed}/images/random`)
-        .then(response => response.json())
-        .then(responseJson => renderDogs(responseJson))
-        .catch(error => alert('Breed not recognized. Please try again.'));
+  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+    .then((response) => response.json())
+    .then((responseJson) => renderDogs(responseJson))
+    .catch((error) => alert("Breed not recognized. Please try again."));
 }
 
 function renderDogs(responseJson) {
-console.log(responseJson);
-$('section').html(`
+  if (responseJson.code === 404) {
+    console.log(responseJson);
+    $("section").html(`
+        <h2>Sorry, that breed could not be found. Please try again.</h2>
+        `);
+  } else {
+    console.log(responseJson);
+    $("section").html(`
     <img src="${responseJson.message}">
 `);
-
+  }
 }
 
 function watchForm() {
-$('form').submit( e => {
+  $("form").submit((e) => {
     const dogBreed = e.target.dogBreed.value;
     event.preventDefault();
     getDogImage(dogBreed);
-});
+  });
 }
 
-$(watchForm)
+$(watchForm);
